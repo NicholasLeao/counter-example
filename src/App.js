26e243520f572b5fd6ab0b91bc9753276b107b3d
NicholasLeao <tropicaldata@gmail.com>
2022-11-21 20:31:50 -0300

    working counter persist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(() => {
+    return JSON.parse(localStorage.getItem("count"));
+  });
+
+  const onDecrease = (e) => setCount((prev) => prev - 1);
+  const onIncrease = (e) => setCount((prev) => prev + 1);
+
+  useEffect(() => {
+    window.localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <h1>{count}</h1>
+        <button onClick={onIncrease}>+</button>
+        <button onClick={onDecrease}>-</button>
+      </div>
     </div>
   );
 }
